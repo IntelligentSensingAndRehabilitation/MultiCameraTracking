32384ae975c9d1cda959a96bc5573c0a07409e12
James Cotton <peabody124@gmail.com>
2023-03-27 19:36:54 -0500

    GUI: update layout

--- multi_camera/acquisition/fastapi_gui/main.py
+++ multi_camera/acquisition/fastapi_gui/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import concurrent.futures
 import socketio
+import glob
+from datetime import datetime
 import cv2
-import base64
+import os
 import asyncio
 import queue
 
 app = FastAPI()
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 app_asgi = socketio.ASGIApp(sio, app)
@@ -34,8 +34,9 @@ async def read_root(request: Request):
 
 
 class StartData(BaseModel):
-    participant_identifier: str
-    recording_note: str
+    recording_dir: str
+    recording_filename: str
+    config_file: str
 
 
 class ChannelData(BaseModel):
@@ -50,9 +51,7 @@ async def set_channel(data: ChannelData):
 
 @app.post("/start")
 async def start_recording(data: StartData):
-    print("Starting recording")
-    participant_identifier = data.participant_identifier
-    recording_note = data.recording_note
+    print(f"Starting recording {data.recording_dir} {data.recording_filename} {data.config_file}")
 
     loop = asyncio.get_event_loop()
 
@@ -77,6 +76,51 @@ async def set_channel(value: int):
     channel_selector_value = value
     print(f"Setting channel to {value}")
     return {"status": "channel_set"}
+
+
+@app.post("/new_session")
+async def new_session(subject_id: str):
+    date = datetime.date.today().strftime("%Y%m%d")
+    session_dir = f"{subject_id}/{date}"
+    os.makedirs(f"./data/{session_dir}", exist_ok=True)
+    return {"recording_dir": session_dir, "recording_filename": f"{subject_id}_{date}"}
+
+
+@app.post("/new_trial")
+async def new_trial(recording_dir: str, recording_filename: str, config_file: str):
+    acquisition.configure(config_file, os.path.join("./data", recording_dir, recording_filename))
+    loop = asyncio.get_event_loop()
+
+    def receive_frames_wrapper(frames):
+        if frames is not None:
+            loop.create_task(receive_frames(frames))
+
+    await loop.run_in_executor(executor, acquisition.start_acquisition)
+    return {"status": "recording_started"}
+
+
+@app.get("/configs")
+async def get_configs():
+    config_files = glob.glob("/home/cbm/MultiCameraTracking/multi_camera_configs/*.yaml")
+    print(config_files)
+    return JSONResponse(content=config_files)
+
+
+@app.get("/recordings")
+async def get_recordings():
+    recordings = []
+    for root, dirs, files in os.walk("./data"):
+        for file in files:
+            if file.endswith(".avi"):
+                recording_path = os.path.join(root, file)
+                participant, date = os.path.basename(os.path.dirname(recording_path)).split("/")
+                comment_path = recording_path[:-4] + ".txt"
+                comment = ""
+                if os.path.exists(comment_path):
+                    with open(comment_path) as f:
+                        comment = f.read()
+                recordings.append({"participant": participant, "filename": file, "comment": comment})
+    return JSONResponse(content=recordings)
 
 
 def downsample_image(image, new_width, new_height):
@@ -108,7 +152,6 @@ async def video_endpoint():
             # Wait for the next frame to become available
             frame = preview_queue.get()
 
-            print("yielding")
             # Write the boundary frame to the response
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
         print("finished generating frames")
