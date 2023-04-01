cdf10a906d20d83474de88738887bdfbff8f61b1
James Cotton <peabody124@gmail.com>
2023-04-01 14:49:40 -0500

    WIP: navbar broken but running

---
+++ react_frontend/src/AcquisitionHome.js
@@ -0,0 +1,28 @@
+import { Container } from 'react-bootstrap';
+import Config from './components/Config';
+import Participant from './components/Participant';
+import RecordingInfo from './components/RecordingInfo';
+import RecordingControl from './components/RecordingControl';
+import Video from './components/Video';
+import CameraStatusTable from './components/CameraStatusTable';
+import PriorRecordingsTable from './components/PriorRecordingsTable';
+import { AquisitionApi } from './AcquistionApi';
+
+const AcquisitionHome = () => {
+    return (
+        <Container style={{ maxWidth: "1000px" }}>
+
+            <AquisitionApi>
+                <Config />
+                <Participant />
+                <RecordingInfo />
+                <RecordingControl />
+                <Video />
+                <PriorRecordingsTable />
+                <CameraStatusTable />
+            </AquisitionApi>
+        </Container>
+    )
+}
+
+export default AcquisitionHome;
