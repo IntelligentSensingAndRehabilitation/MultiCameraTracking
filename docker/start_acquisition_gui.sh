58ab8bd83125b25916cea4988f97debe17ac6117
Kunal Shah <kshah@ricres.org>
2023-06-22 11:36:25 -0500

    Docker: Can record from one camera
    This code needs to be cleaned up from various hacks.
    Changes in this commit reordering Dockerfile, adding
    a local datajoint_config (not committed) to replace the
    default one from the posepipe image. Adding the config
    file (not committed) into the container. Updating the
    bash script run at the end of the Dockerfile to run both
    the frontend and backend. Updating the path that the
    backend looks in for config files. Updating base URL
    for frontend. Updating allowed origins in fastapi.py

--- docker/start_acquisition_gui.sh
+++ docker/start_acquisition_gui.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
 # Start backend
-python3 -m multi_camera.backend.fastapi
+python3 -m multi_camera.backend.fastapi &
 
-#cd react_frontend && npm start &
+cd /Mocap/react_frontend && npm start 
