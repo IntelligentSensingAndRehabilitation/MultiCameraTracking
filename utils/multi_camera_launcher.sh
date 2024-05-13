8920bb5c22d5ebc0792694e45cb4d3fca634b4bd
Kunal Shah <kshah@sralab.org>
2024-05-13 17:32:43 -0500

    startup_utils: Closing open screen sessions before starting

--- utils/multi_camera_launcher.sh
+++ utils/multi_camera_launcher.sh
@@ -4,6 +4,9 @@
 chmod +x /home/cbm/MultiCameraTracking/set_mtu.sh
 /home/cbm/MultiCameraTracking/set_mtu.sh
 
+# Close any open screen sessions with "multi_camera" in their name
+screen -ls | grep -o '[0-9]*\.multi_camera' | xargs -I{} screen -S {} -X quit
+
 # Open a screen session and run the docker container
 gnome-terminal -- bash -c 'screen -S multi_camera -m bash -c "cd /home/cbm/MultiCameraTracking && make run; exec bash"'
 
