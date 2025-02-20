52860b53f0eb2a6b62da8d1f5cce9652d8eac9b2
Kunal Shah <kshah@sralab.org>
2025-02-20 16:56:47 -0600

    session_cal: Updating checks and importing schema

--- multi_camera/datajoint/session_calibrations.py
+++ multi_camera/datajoint/session_calibrations.py
@@ -2,9 +2,7 @@ import datajoint as dj
 
 from .calibrate_cameras import Calibration
 from .multi_camera_dj import MultiCameraRecording, CalibratedRecording
-from .sessions import Session, Recording
-
-schema = dj.schema("multicamera_session_calibrations")
+from .sessions import schema, Session, Recording
 
 @schema
 class SessionCalibration(dj.Computed):
@@ -30,10 +28,12 @@ class SessionCalibration(dj.Computed):
     def make(self, key):
 
         # Get a count of the number of calibrations associated with each recording
-        calibrations_per_recording = Recording.aggr(CalibratedRecording, n="count(*)")
+        session_recording = MultiCameraRecording * Recording & key
+        calibrations_per_recording = Recording.aggr(CalibratedRecording & session_recording, n="count(*)")
 
         # See if there are any recordings with more or less than one calibration
-        assert len(calibrations_per_recording & "n != 1") == 0, "Some recordings are not associated with exactly one calibration"
+        assert len(calibrations_per_recording & "n != 1") == 0, "Some recordings are associated with more than one calibration"
+        assert len(calibrations_per_recording) == len(session_recording), "Some recordings are not associated with a calibration"
 
         # insert the key into the SessionCalibration table
         self.insert1(key)
